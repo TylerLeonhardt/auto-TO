c020cd74a9d4f42c6dd55f2f3a2d018dc80b8da6
Zach Miller <zmmille2@gmail.com>
2019-05-24 21:46:37 -0700

    basic functionality

--- AuTO/IHateNamingThings.cs
+++ AuTO/IHateNamingThings.cs
@@ -1,7 +1,6 @@
 namespace IHateNamingThings
 {
     using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Azure.WebJobs;
     using Microsoft.Azure.WebJobs.Extensions.Http;
     using Microsoft.Extensions.Logging;
@@ -10,7 +9,10 @@ namespace IHateNamingThings
     using System.Net.Http;
     using System.Text;
     using System.Threading.Tasks;
+    using Twilio;
+    using Twilio.Rest.Api.V2010.Account;
     using Twilio.TwiML;
+    using Twilio.Types;
 
     public class IHateNamingThings
     {
@@ -30,6 +32,43 @@ namespace IHateNamingThings
 
             var sender = formValues["From"];
 
+            // Isabela-proofing
+            string authorized = Environment.GetEnvironmentVariable("nextPlayer1");
+
+            if(sender.Substring(1) != authorized.Substring(1))
+            {
+                var error = new MessagingResponse()
+                    .Message($"DON'T @ ME!!  You sent {formValues["Body"]}");
+                var errorTwiml = error.ToString();
+                errorTwiml = errorTwiml.Replace("utf-16", "utf-8");
+
+                return new HttpResponseMessage
+                {
+                    Content = new StringContent(errorTwiml.ToString(), Encoding.UTF8, "application/xml")
+                };
+            }
+
+            string accountId = Environment.GetEnvironmentVariable("accountId");
+            string authToken = Environment.GetEnvironmentVariable("authToken");
+            var myNumber = new PhoneNumber(Environment.GetEnvironmentVariable("myNumber"));
+            var nextPlayer1 = new PhoneNumber(Environment.GetEnvironmentVariable("nextPlayer1"));
+            var nextPlayer2 = new PhoneNumber(Environment.GetEnvironmentVariable("nextPlayer2"));
+            string nextTV = "";
+
+            TwilioClient.Init(accountId, authToken);
+
+            var p2notification = MessageResource.Create(
+                body: $"You're up next on tv {nextTV} against {nextPlayer1}",
+                from: myNumber,
+                to: nextPlayer2);
+
+            var p1notification = MessageResource.Create(
+                body: $"You're up next on tv {nextTV} against {nextPlayer2}",
+                from: myNumber,
+                to: nextPlayer1);
+
+            TwilioClient.Invalidate();
+
             // lookup the bracket
             // get the sender's game
             // assume sender won
@@ -38,14 +77,14 @@ namespace IHateNamingThings
             // update database
             // Perform calculations, API lookups, etc. here
 
-            var response = new MessagingResponse()
-                .Message($"You said: {formValues["Body"]}");
-            var twiml = response.ToString();
-            twiml = twiml.Replace("utf-16", "utf-8");
+            var congrats = new MessagingResponse()
+                .Message($"Congrats on winning!  You sent {formValues["Body"]}");
+            var congratsTwiml = congrats.ToString();
+            congratsTwiml = congratsTwiml.Replace("utf-16", "utf-8");
 
             return new HttpResponseMessage
             {
-                Content = new StringContent(twiml, Encoding.UTF8, "application/xml")
+                Content = new StringContent(congratsTwiml.ToString(), Encoding.UTF8, "application/xml")
             };
         }
     }
