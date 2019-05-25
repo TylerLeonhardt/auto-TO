d3a9e9dde4e3617247f9c1f0873660e3a1cf3668
Tyler Leonhardt <tylerl0706@gmail.com>
2019-05-24 22:11:21 -0700

    data classes

---
+++ AuTO/Schemas/Matches.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace WorldsFirst.Schemas
+{
+    internal class Matches
+    {
+        private List<string> _matchIds = new List<string>();
+        internal List<string> MatchIds => _matchIds;
+    }
+}
